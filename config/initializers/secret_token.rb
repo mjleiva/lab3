d0d4269be2070577796a8ad0d426e2b262e92a14
root <root@gio-VirtualBox.(none)>
2011-06-08 15:53:23 -0600

    tercer test

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Lab3::Application.config.secret_token = '6bad421e8941d72dac8f3cfef4e26f33552c547acd26c2d66ea98ded295e35bcd5039bcc9c8c94078c19a55c620ea7997d9eed079d08a1bdf436178371dd7653'
